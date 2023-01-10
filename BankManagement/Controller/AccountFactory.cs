c79f2d8c05189f4c66cbb5ffe22273db813af1ee
kavya-murali-offl <113909494+kavya-murali-offl@users.noreply.github.com>
2023-01-10 16:30:10 +0530

    Add files via upload

--- BankManagement/Controller/AccountFactory.cs
+++ BankManagement/Controller/AccountFactory.cs
@@ -3,23 +3,25 @@ using BankManagement.Model;
 using BankManagement.Models;
 using BankManagement.Utility;
 using System;
+using System.Security.Principal;
 
 namespace BankManagement.Controller
 {
     public static class AccountFactory
     {
 
-        public static Account GetAccountByType(string accountType)
+        public static Account GetAccountByType(AccountTypes accountType)
         {
-            if (accountType == "CURRENT")
+            if (accountType == AccountTypes.CURRENT)
             {
                 Helper helper = new Helper();
                 decimal amount = helper.GetAmount();
                 CurrentAccount currentAccount = new CurrentAccount();
-                currentAccount.Balance= amount; 
+                currentAccount.Balance= amount;
+                currentAccount.Transactions.Add(new Transaction(amount, amount, TransactionTypes.DEPOSIT));
                 return currentAccount;
             }
-            else if (accountType == "SAVINGS")
+            else if (accountType == AccountTypes.SAVINGS)
             {
                 SavingsAccount savingsAccount = new SavingsAccount();
                 return savingsAccount;
