73768d35dd589968a2d3c987a3be2de95ed87ff0
kavya-murali-offl <113909494+kavya-murali-offl@users.noreply.github.com>
2023-01-10 16:31:30 +0530

    Add files via upload

--- BankManagement/Utility/Printer.cs
+++ BankManagement/Utility/Printer.cs
@@ -1,17 +1,28 @@
 ﻿using BankManagement.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
 
 namespace BankManagement.Utility
 {
     public class Printer
     {
-        public static void PrintStatement(IList<Model.Transaction> transactions)
+        public static void PrintStatement(IList<Transaction> transactions)
         {
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:\\Users\\kavya-pt6688\\source\\repos\\BankManagementApp\\Statement.txt");
+                sw.WriteLine("S.No.\t\tTransaction ID\t\t\t\t\t\t\tTransaction Time\t\t\t\t\t\t\tTransaction Type\t\tAmount\t\tBalance");
+                for(int i = 0;i< transactions.Count;i++) {
+                    sw.WriteLine(i + 1 + "\t\t" + transactions[i].ToString() + "\n\n================\n\n");
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
